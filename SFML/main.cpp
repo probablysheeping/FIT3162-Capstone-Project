450d834dde149008e430afa421ae3154dcdb8760
b-peng <brendan.peng@outlook.com>
2025-09-15 22:03:27 +1000

    removed forced set colour from main.cpp

--- SFML/main.cpp
+++ SFML/main.cpp
@@ -364,7 +364,6 @@ void runProgram()
                     intersection = intersectingPolygon(&intersection, &polygons.at(selectedPolygons.at(i)));
                 }
 
-                intersection.setColour(polygonColour);
                 polygons.push_back(intersection);
 
                 // TODO: Calculate IoU Metric and display result.
